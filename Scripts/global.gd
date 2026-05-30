ed08503412ef6135b2e92b8c0502ca7d6f2bc6d2
Robotathand <97321601+robotathand@users.noreply.github.com>
2026-05-30 17:31:57 +0100

    start writing win condition check for up component of vertical
    thinking of changing it to a function later

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -5,20 +5,38 @@ class GameSetup:
 	var width: int = 15
 	var height: int = 15
 	var length: int = 5
+	# Could add number of lines later on
 
 
 class GameState:
 	# 0 - N/A, -1 - Player A, +1 - Player B
+	var game_setup: GameSetup
 	var game_state: Array[Array] = []
 	var current_player: int = 0
 	var winner: int = 0
 	
-	func set_game_state(current_game: GameSetup):
+	func _init():
+		# Setup game_state array
 		var row: Array[int] = []
-		for _w in current_game.width:
+		for _w in game_setup.width:
 			row.append(0)
-		for _h in current_game.height:
+		for _h in game_setup.height:
 			game_state.append(row)
 	
 	func check_win(current_place: Vector2i):
-		var a = current_place[2]
+		var is_win: bool = false
+		var current_x = current_place.x
+		var current_y = current_place.y
+		
+		# Vertical
+		var vertical_count = 0
+		for up in range(game_setup.length):
+			if current_y - up < 0:
+				break
+			else:
+				if game_state[current_y - up][current_x] == current_player:
+					vertical_count += 1
+				else:
+					break
+		
+		return is_win
