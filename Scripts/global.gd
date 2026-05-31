2a7a753f61434413c81a74ab9dd931df4eecceec
Robotathand <97321601+robotathand@users.noreply.github.com>
2026-05-31 12:20:57 +0100

    make playing moves & undo/redo & resetting current game functionality

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -7,6 +7,16 @@ class GameSetup:
 	var height: int = 15
 	var length: int = 5
 	# could add number of lines needed later on to make the game harder
+	# could add number of players later on
+
+
+class Move:
+	var player: int = 0
+	var location: Vector2i
+	
+	func _init(move):
+		player = move.player
+		location = move.location
 
 
 # can store the current of a game
@@ -14,16 +24,83 @@ class GameState:
 	# 0 - N/A, -1 - Player A, +1 - Player B
 	var game_setup: GameSetup # holds the game settings used for the current game
 	var game_state: Array[Array] = [] # 2d array of integers indicating the counter on that spot
-	var current_player: int = 0
+	var previous_moves_list: Array[Move] = []
+	var redo_moves_list: Array[Move] = []
+	var number_of_moves: int = 0
+	var current_player: int = -1
+	var game_state_before_reset: GameState = GameState.new()
 	
-	func _init():
+	func _init(chosen_game_setup: GameSetup = game_setup):
 		# setup game_state array
 		var row: Array[int] = []
 		for _w in game_setup.width:
 			row.append(0)
 		for _h in game_setup.height:
 			game_state.append(row)
+		
+		game_setup = chosen_game_setup
 	
+	
+	## playing moves
+	
+	# currently only made for 2 players
+	func next_player(_direction: int = +1):
+		current_player *= -1
+	
+	
+	func play_move(move: Move):
+		game_state[move.location.y][move.location.x] = current_player
+		previous_moves_list.append(Move.new(move))
+		
+		# clear future moves
+		if redo_moves_list != []:
+			redo_moves_list.clear()
+		
+		number_of_moves += 1
+		next_player(+1)
+	
+	
+	## undo/redo moves
+	
+	# undoos moves, usually 1
+	func undo_moves(number_of_undos: int = 1):
+		for _undo_number in range(number_of_undos):
+			if not previous_moves_list.is_empty():
+				var previous_move: Move = previous_moves_list.pop_back()
+				game_state[previous_move.location.y][previous_move.location.x] = 0
+				number_of_moves -= 1
+				next_player(-1)
+			else:
+				break
+	
+	
+	# redoos moves, usually 1
+	func redo_moves(number_of_redos: int = 1):
+		for _redo_number in range(number_of_redos):
+			if not redo_moves_list.is_empty():
+				var redo_move: Move = redo_moves_list.pop_back()
+				if redo_move.player != 0:
+					game_state[redo_move.location.y][redo_move.location.x] = redo_move.player
+				else:
+					# fallback only works for 2 player games
+					game_state[redo_move.location.y][redo_move.location.x] = current_player * -1
+			number_of_moves += 1
+			next_player(-1)
+	
+	
+	## reset current game
+	
+	func reset_game_state():
+		game_state_before_reset = self
+		game_setup = game_setup
+		game_state.clear()
+		previous_moves_list.clear()
+		redo_moves_list.clear()
+		number_of_moves = 0
+		current_player = -1
+	
+	
+	## checking for a win
 	
 	func total_counters_one_direction(change_x: int, change_y: int, current_place: Vector2i) -> int:
 		var count = 0
