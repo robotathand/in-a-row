f83f0249af82f419d818b8e65e6ac5ceb7ed94ba
Robotathand <97321601+robotathand@users.noreply.github.com>
2026-05-31 16:51:01 +0100

    started making resizable game screen
    may have errors
    used AI for inspiration but not using generated code

---
+++ Scripts/horizontal_vertical_container.gd
@@ -0,0 +1,18 @@
+extends BoxContainer
+
+func resize():
+	var screen_size = get_viewport_rect().size
+	if screen_size.x > screen_size.y:
+		vertical = false
+	elif screen_size.x < screen_size.y:
+		vertical = true
+	else:
+		vertical = false
+
+
+func _ready() -> void:
+	resize()
+
+
+func _on_resized() -> void:
+	resize()
